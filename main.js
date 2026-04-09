a39369fe6ae06a25572f207ed0837bf641e6661d
Vincent Harlim <Vincent.Harlim@monash.edu>
2026-04-09 13:25:25 +1000

    Added banners for theses and wide format

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ $(document).ready(function () {
     let url = window.location.href;
 
     let in_design = url.includes("Graphic-Design")
+    let in_thesis = url.includes("Theses")
+    let in_wide_format = url.includes("PostersBannersSigns")
 
     if (in_design) {
         root.style.setProperty("--bg", "url(https://github.com/vh-monash/MPS/blob/main/images/Banner%20-%20Design.png?raw=true)")
+    }
+    else if (in_thesis) {
+        root.style.setProperty("--bg", "url(https://github.com/vh-monash/MPS/blob/main/images/Banner%20-%20Theses.png?raw=true)")
+    }
+    else if (in_wide_format) {
+        root.style.setProperty("--bg", "url(https://github.com/vh-monash/MPS/blob/main/images/Banner%20-%20WF.png?raw=true)")
     }
 })
