a336b6d86f7b68210666d198c50beaa91335e983
Vincent Harlim <Vincent.Harlim@monash.edu>
2026-04-09 13:18:07 +1000

    Moved logic from test.js to main.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-$(document).ready(function () {
-    let root = document.querySelector(":root");
-    let url = window.location.href;
-
-    let in_design = url.includes("Graphic-Design")
-
-    if (in_design) {
-        root.style.setProperty("--bg", "url(https://github.com/vh-monash/MPS/blob/main/images/Banner%20-%20Design.png?raw=true)")
-    }
-})
+alert("test");
