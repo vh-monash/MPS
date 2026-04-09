58a92fbddd5712f95356ee052e16c9d0ed6e371f
Vincent Harlim <Vincent.Harlim@monash.edu>
2026-04-09 13:06:55 +1000

    Banner change on Graphic Design product

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-alert("asd");
+$(document).ready(function () {
+    let root = document.querySelector(":root");
+    let url = window.location.href;
+
+    let in_design = url.includes("Graphic-Design")
+
+    if (in_design) {
+        root.style.setProperty("--bg", "url(https://github.com/vh-monash/MPS/blob/main/images/Banner%20-%20Design.png?raw=true)")
+    }
+})
